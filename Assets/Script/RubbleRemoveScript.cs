922d422d9d4be1ddb00989dc7fcda6813b7580a0
Marcel Jurtz <Marcel Jurtz>
2017-01-17 21:09:18 +0100

    Add Skript to remove rubble when falling

---
+++ Assets/Script/RubbleRemoveScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubbleRemoveScript : MonoBehaviour {
+
+	private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Hit");
+        Destroy(col.gameObject);
+    }
+}
